7f8ebb3aed4a231eeb9d5d97f58d5a2254614fd1
andre <andre@pop-os.localdomain>
2019-02-24 11:41:09 -0300

    change plugins and routes drive

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
+//Application controller
+
 import express from "express";
 import dotenv from "dotenv";
 
-import routes from "./routes.js";
+import { $, plugins, controllers } from "@dekproject/scope";
+import routes from "@dekproject/routes";
 
-try{
-    const proxy = require("./proxy");
-} catch(e){ }
+(async () => {
+    dotenv.config(); //Load .env
 
-dotenv.config();
-let app = express();
+    $.set("app", express());
+    await plugins(process.env.PLUGIN_PATH || "src/plugins");
+    await controllers(process.env.CONTROLLERS_PATH || "src/controllers");
 
-//Import proxy if exists
-if(typeof proxy == "function" && process.env.BACKEND_ALIAS){
-    app.use(process.env.BACKEND_ALIAS, routes());
-    app.use(proxy());
-}
-else{
-    app.use(routes());
-}
+    //Import proxy if exists
+    try{
+        const proxy = require("./proxy");
 
-const PORT = process.env.PORT || 5556;
+        if(typeof proxy == "function" && process.env.BACKEND_ALIAS){
+            $.app.use(process.env.BACKEND_ALIAS, await routes(process.env.ROUTES_PATH || "src/routes"));
+            $.app.use(await proxy());
+        }
+        else{
+            $.app.use(await routes(process.env.ROUTES_PATH || "src/routes"));
+        }
+    } catch(e){
+        $.app.use(await routes(process.env.ROUTES_PATH || "src/routes"));
+    }
 
-app.listen(PORT, function () {
-    console.log(`App listening on port ${PORT}!`);
-});
+    const PORT = process.env.PORT || 5555;
+
+    $.app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}!`);
+    });
+})();
--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { index } from "./controllers/index";
-
-let router = express.Router();
-
-export default () => {
-    router.route('/').get(index).post((req, res) => { res.status(404); });
-    return router;
-}
